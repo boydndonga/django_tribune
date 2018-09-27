86adff4a93a1c75b062a1416aa56342196c1730c
boyd ndonga <boydbugle@gmail.com>
2018-09-27 11:34:24 +0300

    create function to request predated ifo

--- news/views.py
+++ news/views.py
@@ -30,3 +30,23 @@ def news_of_day(request):
         </html>
             '''
     return HttpResponse(html)
+
+def past_days_news(request,past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
