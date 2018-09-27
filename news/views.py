e1900876589139294f11b326eb76a02fa65dc165
boyd ndonga <boydbugle@gmail.com>
2018-09-27 12:46:31 +0300

    clear bug

--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
 import datetime as dt
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
 
 # Create your views here.
 def welcome(request):
