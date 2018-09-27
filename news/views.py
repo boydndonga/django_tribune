606fc5752d753aa464946873665b700fc5c32ca4
boyd ndonga <boydbugle@gmail.com>
2018-09-27 11:06:57 +0300

    create view for date displays

--- news/views.py
+++ news/views.py
@@ -1,6 +1,18 @@
+import datetime as dt
 from django.shortcuts import render
 from django.http  import HttpResponse
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Moringa Tribune')
+
+def news_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
