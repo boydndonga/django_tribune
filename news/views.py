5363f3f85afecfafb5df38ed6c2033e931bb4aad
boyd ndonga <boydbugle@gmail.com>
2018-09-27 15:42:19 +0300

    configure bootstrap

--- news/views.py
+++ news/views.py
@@ -22,14 +22,7 @@ def news_of_day(request):
 
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
     day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render(request, 'all-news/today-news.html', {"date": date,})
 
 def past_days_news(request,past_date):
 
@@ -41,12 +34,7 @@ def past_days_news(request,past_date):
         # Raise 404 error when ValueError is thrown
         raise Http404()
 
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-news/past-news.html', {"date": date})
