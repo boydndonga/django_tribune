bcbd54f8b3a15794f82c990c6d851b768f8b9672
boyd ndonga <boydbugle@gmail.com>
2018-10-02 02:10:35 +0300

    correct syntax in model

--- news/models.py
+++ news/models.py
@@ -30,7 +30,8 @@ class tags(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length=60)
     post = HTMLField()
-    editor = models.ForeignKey(User,on_delete=models.CASCADE)     tags = models.ManyToManyField(tags)
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)     
+    tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
     article_image = models.ImageField(upload_to='articles/', blank=True)
     
