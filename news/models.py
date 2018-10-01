a1903d8fcf61b6c462d0cf357ababdc8ac7ec161
boyd ndonga <boydbugle@gmail.com>
2018-10-02 01:40:37 +0300

    update articles model for normal users

--- news/models.py
+++ news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import datetime as dt
+from django.contrib.auth.models import User
 
 
 class Editor(models.Model):
@@ -26,13 +27,13 @@ class tags(models.Model):
         return self.name
 
 class Article(models.Model):
-    title = models.CharField(max_length =60)
+    title = models.CharField(max_length=60)
     post = models.TextField()
-    editor = models.ForeignKey(Editor)
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
-    article_image = models.ImageField(upload_to = 'articles/')
-
+    article_image = models.ImageField(upload_to='articles/', blank=True)
+    
     @classmethod
     def todays_news(cls):
         today = dt.date.today()
