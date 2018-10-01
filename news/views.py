6338dc9584f68a19c8972463fd61ee6e0a5b0ec1
boyd ndonga <boydbugle@gmail.com>
2018-10-01 23:22:18 +0300

    import missing modules

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 import datetime as dt
 from django.shortcuts import render
 from django.http  import HttpResponse,Http404
+from .models import Article,Editor,tags
 
 # Create your views here.
 def welcome(request):
