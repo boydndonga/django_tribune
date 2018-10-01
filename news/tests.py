eba8acf936d534b6305a6267cee65bb1a6fe998d
boyd ndonga <boydbugle@gmail.com>
2018-10-01 20:09:11 +0300

    create tests for editor model

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Article,tags,Editor
 
-# Create your tests here.
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.james,Editor))
