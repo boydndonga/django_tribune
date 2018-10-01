2ac1502e3874ee5d058a709cbd42fcae45f3300d
boyd ndonga <boydbugle@gmail.com>
2018-10-02 01:55:39 +0300

    create view function to create newsarticle

--- news/views.py
+++ news/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse, Http404,HttpResponseRedirect
 from .models import Article,Editor,
 from .email import send_welcome_email,
 from django.contrib.auth.decorators import login_required
+from .forms import NewArticleForm, NewsLetterForm
+
 
 # Create your views here.
 def welcome(request):
@@ -72,3 +74,18 @@ def article(request,article_id):
     except DoesNotExist:
         raise Http404()
     return render(request,"all-news/article.html", {"article":article})
+
+@login_required(login_url='/accounts/login/')
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('NewsToday')
+
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_article.html', {"form": form})
