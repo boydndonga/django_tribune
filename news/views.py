d965a83b50461e8244e27f9e7720d8e71d314d2c
boyd ndonga <boydbugle@gmail.com>
2018-10-10 21:47:11 +0300

    create api url for new model

--- news/views.py
+++ news/views.py
@@ -9,7 +9,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import MerchSerializer
 
-
+class MerchList(APIView):
+    def get(self, request, format=None):
+        all_merch = MoringaMerch.objects.all()
+        serializers = MerchSerializer(all_merch, many=True)
+        return Response(serializers.data)
+        
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
